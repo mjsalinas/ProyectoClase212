0e500fd46686361a24392f4e17fcb426154bd9b6
fernandop-dev <fernandopuertof@gmail.com>
2025-11-05 23:12:56 -0600

    Agregado formulario de registro de usuario

--- MyBeautyApp/src/navigation/NavigationService.ts
+++ MyBeautyApp/src/navigation/NavigationService.ts
@@ -1,4 +1,24 @@
-import { createNavigationContainerRef } from "@react-navigation/native";
+import {
+  createNavigationContainerRef,
+  NavigationState,
+  PartialState,
+} from "@react-navigation/native";
 import { RootStackParamList } from "./StackNavigator";
 
 export const navigationRef = createNavigationContainerRef<RootStackParamList>();
+
+// Helpers opcionales (por si los quieres usar en otros lugares)
+export function navigate<RouteName extends keyof RootStackParamList>(
+  name: RouteName,
+  params?: RootStackParamList[RouteName]
+) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name as never, params as never);
+  }
+}
+
+export function resetTo(state: NavigationState | PartialState<NavigationState>) {
+  if (navigationRef.isReady()) {
+    navigationRef.reset(state);
+  }
+}
--- MyBeautyApp/src/navigation/StackNavigator.tsx
+++ MyBeautyApp/src/navigation/StackNavigator.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from '../screens/LoginScreen';
-import HomeScreen from '../screens/HomeScreen';
-import TabsNavigator from './TabsNavigator';
-import ServiceRegistryScreen from '../screens/services/ServiceRegistryScreen';
+import LoginScreen from "../screens/LoginScreen";
+import HomeScreen from "../screens/HomeScreen";
+import TabsNavigator from "./TabsNavigator";
+import ServiceRegistryScreen from "../screens/services/ServiceRegistryScreen";
+import UserRegisterScreen from "../screens/UserRegisterScreen";
+
 
 export type RootStackParamList = {
-    Login: undefined,
-    Tabs: {email:string},
-    ServiceRegistry: undefined,
-}
+  Login: undefined;
+  Tabs: { email: string };
+  ServiceRegistry: undefined;
+};
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function StackNavigator() {
-    return(
-        <Stack.Navigator initialRouteName='Login' 
-                            screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Tabs" component={TabsNavigator} />
-            <Stack.Screen name="ServiceRegistry" component={ServiceRegistryScreen} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Tabs" component={TabsNavigator} />
+<Stack.Screen name="ServiceRegistry" component={ServiceRegistryScreen} />
+<Stack.Screen name="UserRegister" component={UserRegisterScreen} />
+
+      {/* Si quisieras ver HomeScreen directo, podrías agregarlo aquí también */}
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+    </Stack.Navigator>
+  );
 }
