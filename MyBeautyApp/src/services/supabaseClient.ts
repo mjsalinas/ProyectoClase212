a14164c07452bdf297b735a1b51fbd88e0c80d28
Luis Montalvo <lmontalvoc26@hotmail.com>
2025-11-30 20:55:49 -0600

    agregando ultimos updates de clase

--- MyBeautyApp/src/services/supabaseClient.ts
+++ MyBeautyApp/src/services/supabaseClient.ts
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = "https://dswmcichzoozguzmcylg.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzd21jaWNoem9vemd1em1jeWxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjU4NTgsImV4cCI6MjA3OTYwMTg1OH0.XilmAbcb5kFTHAHEpNdnJmK41i6Smnlu6Ey2ouBJX2g"
-
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-=======
-// tarea: implementar .env
-
 import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://enfiuhpcusasycnmrihv.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuZml1aHBjdXNhc3ljbm1yaWh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjIyNTYsImV4cCI6MjA3OTU5ODI1Nn0.RVH9CvK2pSx8CCWIiWOmBSmy-P6zREdYeyaHmCJ3YHg"
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
 export const supabase = createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
->>>>>>> origin/mybeautyapp-refactor
